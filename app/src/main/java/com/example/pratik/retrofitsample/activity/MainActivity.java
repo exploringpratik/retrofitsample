040c353ed5dd28b129caf1214b64262c04bc8a1b
Pratik Gautam <exploringpratik@gmail.com>
2018-04-29 08:24:52 +0545

    Initial commit

---
+++ app/src/main/java/com/example/pratik/retrofitsample/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pratik.retrofitsample.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pratik.retrofitsample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
